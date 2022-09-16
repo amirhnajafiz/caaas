41f3f70014b18c0497e4b3f674928e431d3e6875
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 08:59:49 +0430

    feat: direction enum

--- src/snake.rs
+++ src/snake.rs
@@ -3,3 +3,23 @@ use piston_window::{Context, G2d};
 use piston_window::types::Color;
 
 use draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
