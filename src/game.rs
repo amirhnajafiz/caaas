07563249d8046a7c2b473ec665bd3c232fa36c66
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 09:49:01 +0430

    add: game struct

--- src/game.rs
+++ src/game.rs
@@ -6,3 +6,24 @@ use rand::{thread_rng, Rng};
 use crate::snake::{Direction, Snake};
 use crate::draw::{draw_block, draw_rectangle};
 
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+    snake: Snake,
+
+    food_exists: bool,
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
