9583015c070f8a139ed73f6a5c95bb76b5ab7973
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 08:54:52 +0430

    init: snake module

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 extern crate piston_window;
 
 mod draw;
+mod snake;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/snake.rs
@@ -0,0 +1,5 @@
+use std::collections::LinkedList;
+use piston_window::{Context, G2d};
+use piston_window::types::Color;
+
+use draw::draw_block;
