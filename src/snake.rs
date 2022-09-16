74aff299b39c4949efdbc3432b8b8df6134d309b
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 09:33:26 +0430

    feat: move forward method

--- src/snake.rs
+++ src/snake.rs
@@ -71,4 +71,36 @@ impl Snake {
 
         return (head_block.x, head_block.y);
     }
+
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        match dir {
+            Some(d) => self.direction = d,
+            None => (),
+        };
+
+        let (last_x, last_y): (i32, i32) = self.head_position();
+
+        let new_block = match self.direction {
+            Direction::Up => Block {
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x - 1,
+                y: last_y,
+            },
+            Direction::Right => Block {
+                x: last_x + 1,
+                y: last_y,
+            }
+        };
+
+        self.body.push_front(new_block);
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
 }
