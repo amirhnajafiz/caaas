53c2353ad86f527715e7e981be00d0afaa2c0dd0
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 09:56:51 +0430

    add: key pressed method

--- src/game.rs
+++ src/game.rs
@@ -27,3 +27,38 @@ pub struct Game {
     game_over: bool,
     waiting_time: f64,
 }
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Game {
+        return Game{
+            snake: Snake::new(2, 2),
+            waiting_time: 0.0,
+            food_exists: true,
+            food_x: 6,
+            food_y: 4,
+            width,
+            height,
+            game_over: false,
+        };
+    }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match Key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None,
+        };
+
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        self.update_snake(dir);
+    }
+}
