38d4b9671ad0587b9aa27634ddb2cb49718f5372
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-16 09:43:01 +0430

    feat: overlap check

--- src/snake.rs
+++ src/snake.rs
@@ -131,4 +131,21 @@ impl Snake {
         let blk = self.tail.clone().unwrap();
         self.body.push_back(blk);
     }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+
+        return false;
+    }
 }
