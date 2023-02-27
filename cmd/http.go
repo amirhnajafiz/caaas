32fdca218c34024c5b97a4a1985b901169c4b57b
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-27 20:49:59 +0330

    add: fiber app

--- cmd/http.go
+++ cmd/http.go
@@ -1,1 +1,18 @@
 package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type HTTP struct{}
+
+func (h HTTP) main(port int) {
+	app := fiber.New()
+
+	if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
+		log.Println(err)
+	}
+}
