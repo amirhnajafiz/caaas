dd9eb3622bac910676624b93a3664b328c7ec9be
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-27 21:45:44 +0330

    add: register model and request

--- internal/model/client.go
+++ internal/model/client.go
@@ -1,1 +1,12 @@
 package model
+
+import (
+	"time"
+)
+
+type Client struct {
+	ID        uint      `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+}
--- internal/port/http/request/register.go
+++ internal/port/http/request/register.go
@@ -1,1 +1,6 @@
 package request
+
+type Register struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
