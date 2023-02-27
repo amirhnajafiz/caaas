96b72ec18ccd5afc151e84d6058d44f33ff2a45a
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-27 21:47:25 +0330

    add: app model

--- internal/model/app.go
+++ internal/model/app.go
@@ -1,1 +1,12 @@
 package model
+
+import "time"
+
+type App struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Key       string    `json:"key"`
+	URI       string    `json:"uri"`
+	UserID    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
