1de6219b68282035670117fd5f2fdba62ce96bb5
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 17:58:24 +0330

    add: storage and configs

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -1,1 +1,10 @@
 package storage
+
+// Config stores storage connection parameters.
+type Config struct {
+	Host     string `koanf:"host"`
+	Port     int    `koanf:"port"`
+	User     string `koanf:"user"`
+	Pass     string `koanf:"pass"`
+	Database string `koanf:"database"`
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,22 @@
 package storage
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// NewConnection to mysql server.
+func NewConnection(cfg Config) (*gorm.DB, error) {
+	address := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.User,
+		cfg.Pass,
+		cfg.Host,
+		cfg.Port,
+		cfg.Database,
+	)
+
+	return gorm.Open(mysql.Open(address), &gorm.Config{})
+}
