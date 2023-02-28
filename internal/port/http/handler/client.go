004700bac06769e36d889cd8e5d40d47f2f0d8b4
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:30:45 +0330

    add: client methods

--- internal/port/http/handler/client.go
+++ internal/port/http/handler/client.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h *Handler) AddClient(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h *Handler) GetAppClient(ctx *fiber.Ctx) error {
+	return nil
+}
