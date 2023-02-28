02d7eb7856f6d222b88a8cdd0d5cab3317ec747d
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:53:43 +0330

    add: clients repository

--- internal/repository/clients/clients.go
+++ internal/repository/clients/clients.go
@@ -1,1 +1,29 @@
 package clients
+
+import (
+	"github.com/amirhnajafiz/authX/internal/model"
+)
+
+type Clients interface {
+	Create(client *model.Client) error
+	GetAppClients(id uint) ([]uint, error)
+	GetSingle(id uint) (*model.Client, error)
+}
+
+func New() Clients {
+	return &clients{}
+}
+
+type clients struct{}
+
+func (a *clients) Create(client *model.Client) error {
+	return nil
+}
+
+func (a *clients) GetAppClients(id uint) ([]uint, error) {
+	return nil, nil
+}
+
+func (a *clients) GetSingle(id uint) (*model.Client, error) {
+	return nil, nil
+}
