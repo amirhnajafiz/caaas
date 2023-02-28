e266c41e3de141e6805c44efc9c2a29fe635c880
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 17:47:25 +0330

    add: create app logic

--- internal/port/http/handler/app.go
+++ internal/port/http/handler/app.go
@@ -1,12 +1,38 @@
 package handler
 
 import (
+	"time"
+
+	"github.com/amirhnajafiz/authX/internal/model"
+	"github.com/amirhnajafiz/authX/internal/port/http/request"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 // CreateApp for a user.
 func (h *Handler) CreateApp(ctx *fiber.Ctx) error {
-	return nil
+	userRequest := new(request.NewApp)
+
+	if err := ctx.BodyParser(&userRequest); err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	appInstance := model.App{
+		Name:      userRequest.Name,
+		Key:       "", // todo: create a api key
+		URI:       "", // todo: create a unique id
+		UserID:    0,  // todo: find user id from token
+		CreatedAt: time.Now(),
+	}
+
+	if err := h.Repository.Apps.Create(&appInstance); err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	return ctx.JSON(fiber.Map{
+		"api_key": appInstance.Key,
+		"uri_id":  appInstance.URI,
+	})
 }
 
 // GetSingleApp of a user.
