89095ca24ee69628941bbc2095ef9d3b926d8ca6
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:31:20 +0330

    add: client docs

--- internal/port/http/handler/client.go
+++ internal/port/http/handler/client.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// AddClient to an app.
 func (h *Handler) AddClient(ctx *fiber.Ctx) error {
 	return nil
 }
 
+// GetAppClient credentials.
 func (h *Handler) GetAppClient(ctx *fiber.Ctx) error {
 	return nil
 }
