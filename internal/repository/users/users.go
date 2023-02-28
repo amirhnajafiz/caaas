53c46998fbb176961a5c91fa2329288597bb468a
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:57:27 +0330

    add: users repository

--- internal/repository/users/users.go
+++ internal/repository/users/users.go
@@ -1,1 +1,29 @@
 package users
+
+import (
+	"github.com/amirhnajafiz/authX/internal/model"
+)
+
+type Users interface {
+	Insert(user *model.User) error
+	GetByID(id uint) (*model.User, error)
+	GetByEmail(email string) (*model.User, error)
+}
+
+func New() Users {
+	return &users{}
+}
+
+type users struct{}
+
+func (u *users) Insert(user *model.User) error {
+	return nil
+}
+
+func (u *users) GetByID(id uint) (*model.User, error) {
+	return nil, nil
+}
+
+func (u *users) GetByEmail(email string) (*model.User, error) {
+	return nil, nil
+}
