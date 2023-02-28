393f7391e126412d89300c1d5e16b8a1d8931576
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:51:05 +0330

    add: apps repository

--- internal/repository/apps/apps.go
+++ internal/repository/apps/apps.go
@@ -1,1 +1,29 @@
 package apps
+
+import (
+	"github.com/amirhnajafiz/authX/internal/model"
+)
+
+type Apps interface {
+	Create(app *model.App) error
+	GetSingle(id uint) (*model.App, error)
+	Delete(id uint) error
+}
+
+func New() Apps {
+	return &apps{}
+}
+
+type apps struct{}
+
+func (a *apps) Create(app *model.App) error {
+	return nil
+}
+
+func (a *apps) GetSingle(id uint) (*model.App, error) {
+	return nil, nil
+}
+
+func (a *apps) Delete(id uint) error {
+	return nil
+}
