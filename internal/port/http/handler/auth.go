020e434000cb54dae9117f4ffe4bfce282ce9179
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:27:23 +0330

    add: signup and login interface

--- internal/port/http/handler/auth.go
+++ internal/port/http/handler/auth.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// Signup a new user into AuthX.
+func (h *Handler) Signup(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// Login a user into AuthX.
+func (h *Handler) Login(ctx *fiber.Ctx) error {
+	return nil
+}
