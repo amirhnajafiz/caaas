9d599e2796694d3e864a88046dc60183ee400e8a
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:29:17 +0330

    add: app methods

--- internal/port/http/handler/app.go
+++ internal/port/http/handler/app.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// CreateApp for a user.
+func (h *Handler) CreateApp(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// GetSingleApp of a user.
+func (h *Handler) GetSingleApp(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// RemoveApp of a user.
+func (h *Handler) RemoveApp(ctx *fiber.Ctx) error {
+	return nil
+}
