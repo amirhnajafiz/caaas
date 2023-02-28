30450675544533c1da29e40d5132c8ffeb37fbe5
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:58:58 +0330

    add: repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"github.com/amirhnajafiz/authX/internal/repository/apps"
+	"github.com/amirhnajafiz/authX/internal/repository/clients"
+	"github.com/amirhnajafiz/authX/internal/repository/users"
+)
+
+// Repository manages the database entities.
+type Repository struct {
+	Apps    apps.Apps
+	Clients clients.Clients
+	Users   users.Users
+}
+
+// New repository.
+func New() Repository {
+	return Repository{
+		Apps:    apps.New(),
+		Clients: clients.New(),
+		Users:   users.New(),
+	}
+}
