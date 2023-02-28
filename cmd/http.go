d8c6b13757727069a72cbb8c1068614b14e6bfb2
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:43:07 +0330

    add: http instance for routes

--- cmd/http.go
+++ cmd/http.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/amirhnajafiz/authX/internal/port/http/handler"
+	"github.com/amirhnajafiz/authX/internal/port/http/middleware"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+// HTTP command.
 type HTTP struct{}
 
+// main function of http command.
 func (h HTTP) main(port int) {
 	app := fiber.New()
+
+	handlerInstance := handler.Handler{}
+	middlewareInstance := middleware.Middleware{}
+
+	app.Post("/api/login", handlerInstance.Login)
+	app.Put("/api/signup", handlerInstance.Signup)
+
+	v1 := app.Use(middlewareInstance.Authenticate)
+
+	v1.Put("/api/app", handlerInstance.CreateApp)
+
+	v1.Get("/api/app/:app_id", handlerInstance.GetAppClient)
+	v1.Delete("/api/app/:app_id", handlerInstance.RemoveApp)
+
+	v1.Put("/api/app/:app_id/client", handlerInstance.AddClient)
+	v1.Get("/api/app/:app_id/client/:client_id", handlerInstance.GetAppClient)
 
 	if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
 		log.Println(err)
