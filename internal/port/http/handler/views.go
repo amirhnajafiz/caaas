325345971525b26ad12d7ad94937d2fcf8d37dd0
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-28 10:45:23 +0330

    add: views methods

---
+++ internal/port/http/handler/views.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// LoginView returns the login view.
+func (h *Handler) LoginView(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// SignupView returns the signup view.
+func (h *Handler) SignupView(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// HomeView returns the home page of application.
+func (h *Handler) HomeView(ctx *fiber.Ctx) error {
+	return nil
+}
