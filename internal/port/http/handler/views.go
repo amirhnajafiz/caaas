7c53e50a608c300a9bae165387b228134ee3fb9c
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-01 19:42:24 +0330

    remove: unnecessary files

--- internal/port/http/handler/views.go
+++ internal/port/http/handler/views.go
@@ -6,29 +6,21 @@ import (
 
 // RootView of web application.
 func (h *Handler) RootView(ctx *fiber.Ctx) error {
-	return ctx.Render("root", fiber.Map{
-		"uri": ctx.Hostname(),
-	})
+	return ctx.Render("root", nil)
 }
 
 // LoginView returns the login view.
 func (h *Handler) LoginView(ctx *fiber.Ctx) error {
-	return ctx.Render("login", fiber.Map{
-		"uri": ctx.Hostname(),
-	})
+	return ctx.Render("login", nil)
 }
 
 // SignupView returns the signup view.
 func (h *Handler) SignupView(ctx *fiber.Ctx) error {
-	return ctx.Render("signup", fiber.Map{
-		"uri": ctx.Hostname(),
-	})
+	return ctx.Render("signup", nil)
 }
 
 // HomeView returns the home page of application.
 func (h *Handler) HomeView(ctx *fiber.Ctx) error {
 	// todo: get user apps
-	return ctx.Render("home", fiber.Map{
-		"uri": ctx.Hostname(),
-	})
+	return ctx.Render("home", nil)
 }
