bc62083d27f627ae1d9f68a71b13cc97da34e3a8
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-01 19:41:06 +0330

    add: hostname as uri to templates

--- internal/port/http/handler/views.go
+++ internal/port/http/handler/views.go
@@ -6,21 +6,29 @@ import (
 
 // RootView of web application.
 func (h *Handler) RootView(ctx *fiber.Ctx) error {
-	return ctx.Render("root", nil)
+	return ctx.Render("root", fiber.Map{
+		"uri": ctx.Hostname(),
+	})
 }
 
 // LoginView returns the login view.
 func (h *Handler) LoginView(ctx *fiber.Ctx) error {
-	return ctx.Render("login", nil)
+	return ctx.Render("login", fiber.Map{
+		"uri": ctx.Hostname(),
+	})
 }
 
 // SignupView returns the signup view.
 func (h *Handler) SignupView(ctx *fiber.Ctx) error {
-	return ctx.Render("signup", nil)
+	return ctx.Render("signup", fiber.Map{
+		"uri": ctx.Hostname(),
+	})
 }
 
 // HomeView returns the home page of application.
 func (h *Handler) HomeView(ctx *fiber.Ctx) error {
 	// todo: get user apps
-	return ctx.Render("home", nil)
+	return ctx.Render("home", fiber.Map{
+		"uri": ctx.Hostname(),
+	})
 }
