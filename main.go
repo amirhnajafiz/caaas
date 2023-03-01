23676569e19443220852e317bb8aad762ba623f3
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-01 16:11:46 +0330

    add: root to main

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"github.com/amirhnajafiz/authX/cmd"
+	"github.com/amirhnajafiz/authX/internal/config"
+	"github.com/amirhnajafiz/authX/pkg/logger"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
 func main() {
-	// TODO: SQL Migrator
-	// TODO: Fiber HTTP server
+	root := cobra.Command{}
+
+	// load configs
+	cfg := config.LoadConfigs()
+
+	// create logger
+	l := logger.NewLogger(cfg.Logger)
+
+	root.AddCommand(
+		cmd.HTTP{
+			Logger: l.Named("http"),
+			Cfg:    cfg,
+		}.Command(),
+		cmd.Migrate{
+			Logger: l.Named("migrate"),
+			Cfg:    cfg,
+		}.Command(),
+	)
+
+	if err := root.Execute(); err != nil {
+		l.Error("failed to execute root", zap.Error(err))
+	}
 }
