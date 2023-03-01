05389152d8e0c3b6333cf464cedff778d471f0e0
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-01 16:28:13 +0330

    add: veiws handlers

--- internal/port/http/handler/views.go
+++ internal/port/http/handler/views.go
@@ -4,17 +4,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// RootView of web application.
+func (h *Handler) RootView(ctx *fiber.Ctx) error {
+	return ctx.Render("root", nil)
+}
+
 // LoginView returns the login view.
 func (h *Handler) LoginView(ctx *fiber.Ctx) error {
-	return nil
+	return ctx.Render("login", nil)
 }
 
 // SignupView returns the signup view.
 func (h *Handler) SignupView(ctx *fiber.Ctx) error {
-	return nil
+	return ctx.Render("signup", nil)
 }
 
 // HomeView returns the home page of application.
 func (h *Handler) HomeView(ctx *fiber.Ctx) error {
-	return nil
+	// todo: get user apps
+	return ctx.Render("home", nil)
 }
