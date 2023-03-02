a2265f83b3ea26355bad641e427ed1344f58d46f
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-02 15:39:47 +0330

    update: apps repository

--- internal/repository/apps/apps.go
+++ internal/repository/apps/apps.go
@@ -8,7 +8,8 @@ import (
 
 type Apps interface {
 	Create(app *model.App) error
-	GetSingle(id uint) (*model.App, error)
+	GetSingle(key string) (*model.App, error)
+	GetUserApps(userID uint) ([]*model.App, error)
 }
 
 func New(db *gorm.DB) Apps {
@@ -25,12 +26,22 @@ func (a *apps) Create(app *model.App) error {
 	return a.db.Create(app).Error
 }
 
-func (a *apps) GetSingle(id uint) (*model.App, error) {
+func (a *apps) GetSingle(key string) (*model.App, error) {
 	app := new(model.App)
 
-	if err := a.db.First(&app).Where("id = ?", id).Error; err != nil {
+	if err := a.db.First(&app).Where("app_key = ?", key).Error; err != nil {
 		return nil, err
 	}
 
 	return app, nil
+}
+
+func (a *apps) GetUserApps(userID uint) ([]*model.App, error) {
+	var list []*model.App
+
+	if err := a.db.Find(list).Where("user_id = ?", userID).Error; err != nil {
+		return nil, err
+	}
+
+	return list, nil
 }
