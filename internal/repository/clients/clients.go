4faf4acb83a45423a7b993e27b0b97b2503abcfe
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-02 15:43:01 +0330

    add: client repository methods

--- internal/repository/clients/clients.go
+++ internal/repository/clients/clients.go
@@ -23,13 +23,25 @@ type clients struct {
 }
 
 func (a *clients) Create(client *model.Client) error {
-	return nil
+	return a.db.Create(client).Error
 }
 
 func (a *clients) GetAppClients(id uint) ([]uint, error) {
-	return nil, nil
+	var list []uint
+
+	if err := a.db.Raw("select id from clients where app_id = ?", id).Find(list).Error; err != nil {
+		return nil, err
+	}
+
+	return list, nil
 }
 
 func (a *clients) GetSingle(id uint) (*model.Client, error) {
-	return nil, nil
+	client := new(model.Client)
+
+	if err := a.db.Find(client).Where("id = ?", id).Error; err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
