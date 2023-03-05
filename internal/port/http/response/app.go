efe53f9caac0020a5c4481ddcf70241c4b5f5c69
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-05 18:49:47 +0330

    remove: response

--- internal/port/http/response/app.go
+++
@@ -1,8 +0,0 @@
-package response
-
-type App struct {
-	Name      string `json:"name"`
-	AppKey    string `json:"app_key"`
-	URL       string `json:"url"`
-	CreatedAt string `json:"created_at"`
-}
--- internal/port/http/response/client.go
+++
@@ -1,6 +0,0 @@
-package response
-
-type Client struct {
-	Claims    string `json:"claims"`
-	CreatedAt string `json:"created_at"`
-}
