6f297a601a7f16e4e3ec0402508a7bea0b9f1d44
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-05 18:16:22 +0330

    update: apps repository

--- internal/repository/apps/apps.go
+++ internal/repository/apps/apps.go
@@ -6,42 +6,24 @@ import (
 	"gorm.io/gorm"
 )
 
+// Apps manages the apps model.
 type Apps interface {
 	Create(app *model.App) error
-	GetByKey(key string) (*model.App, error)
-	GetByUserID(userID uint) ([]*model.App, error)
 }
 
+// New generates new app repository.
 func New(db *gorm.DB) Apps {
 	return &apps{
 		db: db,
 	}
 }
 
+// apps manages the functions of repository.
 type apps struct {
 	db *gorm.DB
 }
 
+// Create a new app.
 func (a *apps) Create(app *model.App) error {
 	return a.db.Create(app).Error
-}
-
-func (a *apps) GetByKey(key string) (*model.App, error) {
-	app := new(model.App)
-
-	if err := a.db.First(&app).Where("app_key = ?", key).Error; err != nil {
-		return nil, err
-	}
-
-	return app, nil
-}
-
-func (a *apps) GetByUserID(userID uint) ([]*model.App, error) {
-	var list []*model.App
-
-	if err := a.db.Find(&list).Where("user_id = ?", userID).Error; err != nil {
-		return nil, err
-	}
-
-	return list, nil
 }
