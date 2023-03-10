bd0762b80006398dd4e07d98ecc5cd36cd99ab1c
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-10 18:46:07 +0330

    add: version script

--- build/release/release.sh
+++ build/release/release.sh
@@ -3,6 +3,8 @@
 echo "Terminating" $2;
 echo "Release" $1;
 
-docker stop authx:$2;
-docker build . -f build/package/Dockerfile -t authx:$1;
-docker run -d -p 8080:8080 authx:$1;
+sudo docker stop authx:$2;
+sudo docker build . -f build/package/Dockerfile -t authx:$1;
+sudo docker run -d -p 8080:8080 authx:$1;
+
+sudo docker ps;
