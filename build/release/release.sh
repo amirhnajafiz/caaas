4a63975c6739f5e691dca292a7567b6c7d71584c
amirhnajafiz <najafizadeh21@gmail.com>
2023-03-10 18:47:08 +0330

    update: release script

--- build/release/release.sh
+++ build/release/release.sh
@@ -3,8 +3,8 @@
 echo "Terminating" $2;
 echo "Release" $1;
 
-sudo docker stop authx:$2;
+sudo docker stop authx-container;
 sudo docker build . -f build/package/Dockerfile -t authx:$1;
-sudo docker run -d -p 8080:8080 authx:$1;
+sudo docker run -d -p 8080:8080 authx:$1 --name authx-container;
 
 sudo docker ps;
