308d55227e8707da82c6efd358248a87a5246376
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-20 19:27:06 +0330

    add: controller

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -2,6 +2,13 @@ package controller
 
 import "github.com/go-pg/pg/v10"
 
+// Controller is responsible for making database connections.
 type Controller struct {
 	database *pg.DB
+}
+
+func NewController(db *pg.DB) *Controller {
+	return &Controller{
+		database: db,
+	}
 }
