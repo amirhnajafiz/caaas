26d30e4f25f5eea14f4c2391b09af8044e1724e7
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-20 19:33:27 +0330

    add: migrate handler

---
+++ internal/handlers/migrate/handler.go
@@ -0,0 +1,30 @@
+package migrate
+
+import (
+	"github.com/amirhnajafiz/caaas/internal/model"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+type Handler struct {
+	database *pg.DB
+}
+
+func (h Handler) Execute() error {
+	models := []interface{}{
+		(*model.User)(nil),
+		(*model.UserGroup)(nil),
+	}
+
+	for _, model := range models {
+		err := h.database.Model(model).CreateTable(&orm.CreateTableOptions{
+			Temp: true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
