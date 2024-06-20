3b5bf8c3327454245b317bf0fba414ad87324666
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-20 18:38:55 +0330

    set: model tags

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,12 @@
 package model
 
+import "time"
+
+// Each user has a unique username and a password.
 type User struct {
-	ID       uint   `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID        int64     `pg:"id"`
+	Username  string    `pg:"username,unique"`
+	Password  string    `pg:"password"`
+	CreatedAt time.Time `pg:"created_at"`
+	UpdatedAt time.Time `pg:"updated_at"`
 }
--- internal/model/user_group.go
+++ internal/model/user_group.go
@@ -1,6 +1,11 @@
 package model
 
+import "time"
+
+// UserGroup reperents a relationship between each user and a group.
 type UserGroup struct {
-	UserID    uint   `json:"user_id"`
-	GroupName string `json:"group_name"`
+	UserID    int64     `pg:"user_id,unique:usergroup"`
+	GroupName string    `pg:"group_name,unique:usergroup"`
+	CreatedAt time.Time `pg:"created_at"`
+	UpdatedAt time.Time `pg:"updated_at"`
 }
