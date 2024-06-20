dacd5b56406188b15659586fb1bc1696452a1c2b
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-20 16:59:40 +0330

    update: models

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,14 +1,3 @@
 package model
 
-import (
-	"gorm.io/gorm"
-)
-
-// User
-// each user is a student that we store
-// their student number and a password.
-type User struct {
-	gorm.Model
-	StudentNumber string `json:"student_number"`
-	Password      string `json:"password"`
-}
+type User struct{}
